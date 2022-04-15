77deb4197392ab087c02c0ad88a6213b6bb5f3fe
guilhermeft <guilhermetrind12@gmail.com>
2022-04-15 01:59:38 -0300

    feat: create theme file and your types

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+const theme = {
+  colors: {
+    text: {
+      primary: '#3A3A3A',
+      secondary: '#424242',
+      tertiary: '#3488E1',
+      quaternary: '#EF4B4B'
+    },
+    background: {
+      gray: '#F4F4F4',
+      white: '#FFFFFF'
+    }
+  }
+}
+
+export default theme
